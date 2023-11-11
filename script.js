980df3e56ea71d275b957e330ca223a05220f3b5
Jasper Wang <jasper8777@icloud.com>
2023-11-11 00:03:45 -0800

    remove comments

--- script.js
+++ script.js
@@ -22,21 +22,6 @@ function renderMovie(data) {
   moviesContainer.insertAdjacentHTML('beforeend', html);
 }
 
-// function displayMovies() {
-//   fetch('https://yts.mx/api/v2/list_movies.json')
-//     .then((response) => {
-//       if (!response.ok) throw new Error('Could not find any movies!');
-//       return response.json();
-//     })
-//     .then(({ data }) => {
-//       console.log(data.movies);
-//       data.movies.forEach((movie) => renderMovie(movie));
-//     })
-//     .finally(() => {
-//       moviesContainer.style.opacity = 1;
-//       loaderContainer.style.display = 'none';
-//     });
-// }
 async function displayMovies(movieData) {
   moviesContainer.innerHTML = '';
   movieData.movies.forEach((movie) => renderMovie(movie));
